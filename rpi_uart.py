26d18c7af1141bfcc1b21c30d7359f91ea87bc2f
Zayda Deese <zedeese123@gmail.com>
2024-11-12 23:13:54 -0500

    updated to confirm rpi transmission is correct testing

--- rpi_uart.py
+++ rpi_uart.py
@@ -20,10 +20,15 @@ uart = serial.Serial(
 # Poll Rx and if signal then send data to STM32
 while True: 
     received = uart.read() # Default Size 1 byte
-    received_data = int.from_bytes(received, byteorder="big")
-    print(f"RPi received {received_data}")
-    if received_data == 1: 
-        # Send data to the STM32
-        data_to_send = "Hello, STM32 Microcontroller"
-        uart.write(data_to_send.encode('utf-8'))  # Send data as bytes using utf-8 
-        print("Data sent to STM32: ", data_to_send)
+    if received: # Check if anything was received
+        received_data = int.from_bytes(received, byteorder="big")
+        print(f"RPi received {received_data}")
+
+        # Check for ready signal from STM32
+        if received_data == 1: 
+            # Send data to the STM32
+            data_to_send = "Hello"
+            encoded_data = data_to_send.encode('utf-8') # Encode as bytes
+            uart.write(encoded_data)  # Send data as bytes using utf-8 
+            print("Data sent to STM32: ", data_to_send)
+            time.sleep(1)
